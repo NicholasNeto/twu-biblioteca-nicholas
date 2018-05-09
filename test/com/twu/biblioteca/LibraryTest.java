341bf86bcd5f5f90cf2f8a1de736c87967e123f3
NicholasNeto <npnicolls@gmail.com>
2018-05-09 14:20:11 -0300

    removendo cdg duplicado , limpando test

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -13,37 +13,47 @@ public class LibraryTest {
     Library library;
     ArrayList<Book> bookList;
     ArrayList<Movie> filmList;
+    Book book;
+
 
     @Before
     public void setUp(){
-        Utilitaria util = new  Utilitaria();
+
         library = new Library();
-        bookList = util.createBookList();
-        filmList = util.createFilmList();
+
+        bookList = new ArrayList<Book>();
+        filmList = new ArrayList<Movie>();
+
+        book = new Book("1", "TDD", "Kent", 2005);
+
+        bookList.add(book);
+        library.setBookList(bookList);
     }
 
 
     @Test
     public void testShowManyBooks(){
-        bookList.add(new Book("1","TDD","Kent", 2005));
-        bookList.add(new Book("2","Design Patterns","Fowler", 2004));
-        library.setBookList(bookList);
+        ArrayList<Book> _bookList = new ArrayList<Book>();
+
+        _bookList.add(new Book("1","TDD","Kent", 2005));
+        _bookList.add(new Book("2","Design Patterns","Fowler", 2004));
+        library.setBookList(_bookList);
 
         assertEquals(String.format("%20s %20s %20s %20d %20s\n%20s %20s %20s %20d %20s", "1",
-                "TDD", "Kent", 2005, "Free", "2", "Design Patterns", "Fowler", 2004, "Free"), library.getMediasAsString(bookList));
+                "TDD", "Kent", 2005, "Free","2", "Design Patterns", "Fowler", 2004, "Free" ), library.getMediasAsString(_bookList));
     }
+
 
     @Test
     public void testGivenBookListShowMediaTable(){
-        bookList.add(new Book("1","TDD","Kent", 2005));
-        library.setBookList(bookList);
+
         assertEquals(String.format("%20s %20s %20s %20s %20s\n%20s %20s %20s %20d %20s",
-                "ID", "Name","Authors", "Years","Status", "1", "TDD", "Kent", 2005, "Free"), library.showMediaInTable(bookList));
+                "ID", "Name","Authors", "Years", "Status", "1", "TDD", "Kent", 2005, "Free"), library.showMediaInTable(bookList));
     }
 
     @Test
     public void testGivenFilmListShowMediaTable(){
-        filmList.add(new Movie("1", "The Matrix", "Nick", 2015, 3));
+        filmList.add(new Movie("1", "The Matrix", "Nick", 2015));
         library.setFilmList(filmList);
         assertEquals(String.format("%20s %20s %20s %20s %20s\n%20s %20s %20s %20d %20s",
                 "ID", "Name","Authors", "Years", "Status", "1", "The Matrix", "Nick", 2015, "Free"), library.showMediaInTable(filmList));
@@ -51,10 +61,7 @@ public class LibraryTest {
 
     @Test
     public void testSucessfulBorrowBook(){
-        Book book = new Book("1", "TDD", "Kent", 2005);
 
-        bookList.add(book);
-        library.setBookList(bookList);
         String borrowMessage = library.borrowLibraryBook("1");
 
         assertEquals("Thank you! Enjoy the book", borrowMessage);
@@ -62,37 +69,26 @@ public class LibraryTest {
 
     @Test
     public void testUnsuccessfulBorrowBook(){
-        Book book = new Book("1", "TDD", "Kent", 2005);
 
-        bookList.add(book);
-        library.setBookList(bookList);
         library.borrowLibraryBook("1");
-
-        assertEquals("This book is not available", library.borrowLibraryBook("2"));
+        assertEquals("This book is not available", library.borrowLibraryBook("3"));
     }
 
     @Test
     public void testSuccessfulReturnBook(){
-        Book book = new Book("1", "TDD", "Kent", 2005);
 
-        bookList.add(book);
-        library.setBookList(bookList);
         library.borrowLibraryBook("1");
-
         assertEquals("Thank you for returning the book", library.returnBookToTheLibrary("1"));
     }
 
     @Test
     public void testUnsuccessfulReturnBook(){
 
-        Book book = new Book("1", "TDD", "Kent", 2005);
-
-        bookList.add(book);
         library.setBookList(bookList);
         library.borrowLibraryBook("1");
         library.returnBookToTheLibrary("1");
 
-        assertEquals("This is not a valid book to return", library.returnBookToTheLibrary("2"));
+        assertEquals("This is not a valid book to return", library.returnBookToTheLibrary("3"));
         assertTrue(library.bookList.contains(book));
     }
 
