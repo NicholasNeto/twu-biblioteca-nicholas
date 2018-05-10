90a3106cf74655b2140d99df56bcca52ffdec750
NicholasNeto <npnicolls@gmail.com>
2018-05-10 15:23:28 -0300

    refatorando nome de variavel e criando metodo de setUp para a classe User

--- test/com/twu/biblioteca/models/test/UserTest.java
+++ test/com/twu/biblioteca/models/test/UserTest.java
@@ -2,19 +2,26 @@ package com.twu.biblioteca.models.test;
 
 
 import com.twu.biblioteca.models.User;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UserTest {
 
+    User userNicholas;
+
+    @Before
+    public void setUp(){
+        userNicholas = new User("1", "Nicholas", "Nicholas@gmail.com", 123456789, "1313");
+    }
+
     @Test
     public void testCreateUser(){
 
-        User user = new User("1", "Nicholas", "Nicholas@gmail.com", 123456789, "1313");
-        assertEquals("1", user.getIdUser());
-        assertEquals("Nicholas", user.getName());
-        assertEquals("Nicholas@gmail.com", user.getEmail());
-        assertEquals(123456789, user.getPhone());
-        assertEquals("1313", user.getPassword());
+        assertEquals("1", userNicholas.getIdUser());
+        assertEquals("Nicholas", userNicholas.getName());
+        assertEquals("Nicholas@gmail.com", userNicholas.getEmail());
+        assertEquals(123456789, userNicholas.getPhone());
+        assertEquals("1313", userNicholas.getPassword());
     }
 }
