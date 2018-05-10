99c2dce8e7d04e96ac505187f78cfe74b73244a5
NicholasNeto <npnicolls@gmail.com>
2018-05-10 15:20:49 -0300

    melhorando teste para o book

--- test/com/twu/biblioteca/models/test/BookTest.java
+++ test/com/twu/biblioteca/models/test/BookTest.java
@@ -1,17 +1,34 @@
 package com.twu.biblioteca.models.test;
 
 import com.twu.biblioteca.models.Book;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BookTest { private Book book;
 
-    @Test
-    public void testCreateBook(){
-        Book book1 = new Book("1","TDD", "Kent", 2005);
-        assertEquals("TDD", book1.getName());
-        assertEquals("Kent", book1.getAuthor());
-        assertEquals(2005, book1.getYears());
+    private Book bookTdd;
+
+    @Before
+    public void setUp(){
+        bookTdd = new Book("1","TDD", "Kent", 2005);
     }
+
+
+    @Test
+    public void shouldSeeABookBeingCreatedSuccessfully(){
+
+        assertEquals("TDD", bookTdd.getName());
+        assertEquals("Kent", bookTdd.getAuthor());
+        assertEquals(2005, bookTdd.getYears());
+    }
+
+    @Test
+    public void shouldSeeTheAttributesOfABookSuccessfully(){
+
+        String expect = String.format("%20s %20s %20s %20d %20s", "1", "TDD", "Kent", 2005, "Free");
+        assertEquals(expect, bookTdd.toString());
+    }
+
 }
