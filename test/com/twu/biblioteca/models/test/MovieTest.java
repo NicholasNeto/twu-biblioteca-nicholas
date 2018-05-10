5e2029a240e8e45d25eaf3481d07ed36f19d9d5b
NicholasNeto <npnicolls@gmail.com>
2018-05-10 15:21:20 -0300

    refatorando teste para Movie

--- test/com/twu/biblioteca/models/test/MovieTest.java
+++ test/com/twu/biblioteca/models/test/MovieTest.java
@@ -1,17 +1,36 @@
 package com.twu.biblioteca.models.test;
 
 import com.twu.biblioteca.models.Movie;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MovieTest {
 
-    @Test
-    public void testCreateBook(){
-        Movie film1 = new Movie("1","The Matrix", "Cara Incrivel", 2015);
-        assertEquals("The Matrix", film1.getName());
-        assertEquals("Cara Incrivel", film1.director);
-        assertEquals(2015, film1.getYears());
+
+    private Movie movieMatrix;
+
+    @Before
+    public void setUp(){
+        movieMatrix = new Movie("1","The Matrix", "Lilly Wachowski", 2015);
     }
+
+    @Test
+    public void shouldSeeAMovieBeingCreatedSuccessfully(){
+
+        assertEquals("The Matrix", movieMatrix.getName());
+        assertEquals("Lilly Wachowski", movieMatrix.director);
+        assertEquals(2015, movieMatrix.getYears());
+    }
+
+
+    @Test
+    public void shouldSeeTheAttributesOfAMovieSuccessfully(){
+
+        String expect = String.format("%20s %20s %20s %20d %20s", "1", "The Matrix", "Lilly Wachowski", 2015, "Free");
+        assertEquals(expect, movieMatrix.toString());
+    }
+
+
 
 }
