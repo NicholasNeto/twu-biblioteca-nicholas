eab8bf775cdff5bffca2316ffae0d49b9ddbfe9d
NicholasNeto <npnicolls@gmail.com>
2018-05-10 15:22:20 -0300

    criando constantes no projeto com o Enum

--- src/com/twu/biblioteca/models/StatusEnum.java
+++ src/com/twu/biblioteca/models/StatusEnum.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca.models;
 
 public enum StatusEnum {
+
+    AVAILABLE("Available"),
+    UNAVAILABLE("Unavailable");
+
+    private String description;
+
+    private StatusEnum(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
 }
