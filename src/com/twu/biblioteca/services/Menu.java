323c30034cdeab1bd983c442367bcc894d0570b5
NicholasNeto <npnicolls@gmail.com>
2018-05-12 09:51:22 -0300

    refatorado os nome das chamadas

--- src/com/twu/biblioteca/services/Menu.java
+++ src/com/twu/biblioteca/services/Menu.java
@@ -1,53 +1,55 @@
 package com.twu.biblioteca.services;
 
+import com.twu.biblioteca.models.ItemsLibrary;
+import com.twu.biblioteca.models.User;
 import com.twu.biblioteca.services.Library;
 
 import java.util.Scanner;
 
 public class Menu {
 
-    Library library;
+    Autheticator autheticator;
 
-    public Menu(Library library) {
-        this.library = library;
+    public Menu() {
+        autheticator = new Autheticator();
     }
 
     public void showWelcome(){
         System.out.println("\tWELCOME");
     }
 
-    public void login(){
+    public User login(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Login:");
-        String nome = scanner.nextLine();
+        String idUser = scanner.nextLine();
 
         System.out.print("Password:");
         String password = scanner.nextLine();
 
-        library.login(nome, password);
+        return autheticator.login(idUser, password);
     }
 
-    public void printMenuItens() {
-        if(library.isLogged()){
+    public void printMenuItens(User user) {
+        if(user.isLogged()){
             System.out.println("\t Menu ");
             System.out.println("Select: \n"
                     + "(1) to browse books" + " - " + "(2) Lend Book" + " - " +  "(3) Return Book" + " - " + "(4) Show books unavailable \n"
                     + "(5) to browse films" + " - " + "(6) Lend film" + " - " +  "(7) Return Film" + " - " + "(8) Show films unavailable \n"
                     + "(0) to exit. \n");
             System.out.println("Option:");
-        } else{
+        } else {
             System.out.println("No menu option avalable");
         }
     }
 
-    public void showOption(int input){
+    public void showOption(int input, Library library){
         if (input >= 0) {
             int option;
             option = input;
-
+            ItemsLibrary itemsLibrary;
             switch (option) {
                 case 1:
-                    System.out.println(library.showMediaInTable(library.bookList));
+                    System.out.println(library.showMediaInTable(library.getItemLibraryAvaible().getBookList()));
                     break;
 
                 case 2:
@@ -55,7 +57,7 @@ public class Menu {
                     Scanner scannerInputBook = new Scanner(System.in);
                     int inputBook = scannerInputBook.nextInt();
                     String inputStringBook= String.valueOf(inputBook);
-                    library.borrowLibraryBook(inputStringBook);
+                    library.borrowLibraryMedia(inputStringBook);
                     break;
 
                 case 3:
@@ -63,15 +65,16 @@ public class Menu {
                     Scanner scannerReturnBook = new Scanner(System.in);
                     int inputReturnBook = scannerReturnBook.nextInt();
                     String inputReturnStringBook = String.valueOf(inputReturnBook);
-                    library.returnBookToTheLibrary(inputReturnStringBook);
+                    library.returMediaToTheLibrary(inputReturnStringBook);
                     break;
 
                 case 4:
-                    System.out.println(library.showUnavailableBook());
+                    itemsLibrary = library.getItemsUnavailable();
+                    System.out.println(library.showMediaInTable(itemsLibrary.getBookList()));
                     break;
 
                 case 5:
-                    System.out.println(library.showMediaInTable(library.filmList));
+                    System.out.println(library.showMediaInTable(library.getItemLibraryAvaible().getMovieList()));
                     break;
 
                 case 6:
@@ -79,7 +82,7 @@ public class Menu {
                     Scanner scannerInputFilm = new Scanner(System.in);
                     int inputFilm = scannerInputFilm.nextInt();
                     String inputStringFilm= String.valueOf(inputFilm);
-                    library.borrowLibraryFilm(inputStringFilm);
+                    library.borrowLibraryMedia(inputStringFilm);
                     break;
 
                 case 7:
@@ -87,11 +90,12 @@ public class Menu {
                     Scanner scannerReturnFilm = new Scanner(System.in);
                     int inputReturnFilm = scannerReturnFilm.nextInt();
                     String inputReturnStringFilm = String.valueOf(inputReturnFilm);
-                    library.returnFilmToTheLibrary(inputReturnStringFilm);
+                    library.returMediaToTheLibrary(inputReturnStringFilm);
                     break;
 
                 case 8:
-                    System.out.println(library.showUnavailableFilm());
+                    itemsLibrary = library.getItemsUnavailable();
+                    System.out.println(library.showMediaInTable(itemsLibrary.getMovieList()));
                     break;
 
                 case 0:
