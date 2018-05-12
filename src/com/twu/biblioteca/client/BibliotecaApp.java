050ac860e29f3e8f33401e969ab1bc64f2863e24
NicholasNeto <npnicolls@gmail.com>
2018-05-12 09:26:46 -0300

    refatorando cdg no app, para criaçao de biblioteca com usuario

--- src/com/twu/biblioteca/client/BibliotecaApp.java
+++ src/com/twu/biblioteca/client/BibliotecaApp.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca.client;
 
+import com.twu.biblioteca.models.User;
 import com.twu.biblioteca.services.Library;
 import com.twu.biblioteca.services.Menu;
 
@@ -8,19 +9,19 @@ import java.util.Scanner;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        Library library = new Library();
 
-        Menu menuItens = new Menu(library);
+        Menu menuItens = new Menu();
         menuItens.showWelcome();
-        menuItens.login();
-        menuItens.printMenuItens();
+        User user =  menuItens.login();
+        Library library = new Library(user);
+        menuItens.printMenuItens(user);
 
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
 
         while (input > 0){
-            menuItens.showOption(input);
-            menuItens.printMenuItens();
+            menuItens.showOption(input, library);
+            menuItens.printMenuItens(user);
             input = scanner.nextInt();
         }
 
