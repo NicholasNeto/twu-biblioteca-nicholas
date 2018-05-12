d003c9f1210fc4f4d5ee82eff1c02e25127b2954
NicholasNeto <npnicolls@gmail.com>
2018-05-12 09:34:57 -0300

    criado um autenticador de usuarios

--- src/com/twu/biblioteca/services/Autheticator.java
+++ src/com/twu/biblioteca/services/Autheticator.java
@@ -1,4 +1,34 @@
 package com.twu.biblioteca.services;
 
+
+import com.twu.biblioteca.models.User;
+import com.twu.biblioteca.util.Utilitaria;
+import javafx.beans.binding.ObjectExpression;
+
+import java.util.ArrayList;
+
 public class Autheticator {
+
+    private User user;
+    Utilitaria util = new Utilitaria();
+    ArrayList<User> userList = util.createListUserAndPassword();
+
+    public User login(String idUser, String password) {
+
+        for (User user : userList) {
+            if (idUser.equals(user.getIdUser()) && password.equals(user.getPassword())) {
+
+                user.setLogged(true);
+                this.user = user;
+                System.out.println("Welcome " + user.getName());
+                System.out.println("Email: " + user.getEmail() + " Phone: " + user.getPhone());
+            }
+        }
+        return user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
 }
