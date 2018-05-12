1ac1e42bca84599e737210b12eb6e6dab4e7e53f
NicholasNeto <npnicolls@gmail.com>
2018-05-12 09:52:20 -0300

    criado lista de item

--- src/com/twu/biblioteca/util/Utilitaria.java
+++ src/com/twu/biblioteca/util/Utilitaria.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca.util;
 
 import com.twu.biblioteca.models.Book;
+import com.twu.biblioteca.models.Item;
 import com.twu.biblioteca.models.Movie;
 import com.twu.biblioteca.models.User;
 
@@ -9,10 +10,27 @@ import java.util.ArrayList;
 public class Utilitaria {
 
     public Utilitaria() {
+        createItemsList();
         createBookList();
         createFilmList();
         createListUserAndPassword();
 
+    }
+
+    public ArrayList<Item> createItemsList() {
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        items.add(new Book("1", "TDD", "Kent", 2000));
+        items.add(new Book("2", "Design Patterns", "Fowler", 2001));
+        items.add(new Book("3", "Harry Potter", "Kent", 2002));
+        items.add(new Book("4", "The Lord of the Rings", "Peter Jackson", 2003));
+
+        items.add(new Movie("5", "A luz", "Nicholas", 2000));
+        items.add(new Movie("6", "Mochila Azul", "Nicols", 2001));
+        items.add(new Movie("7", "Harry Potter", "J. K. Rowling", 2002));
+        items.add(new Movie("8", "The Matrix", "Cara Incrivel", 2003));
+
+        return items;
     }
 
     public ArrayList<Book> createBookList() {
