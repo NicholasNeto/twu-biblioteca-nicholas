4331c659be2fc1107415fbcbf02d641accc4a536
NicholasNeto <npnicolls@gmail.com>
2018-05-12 09:31:44 -0300

    criado um objeto que guarda uma lista de itens , lista generica

--- src/com/twu/biblioteca/models/ItemsLibrary.java
+++ src/com/twu/biblioteca/models/ItemsLibrary.java
@@ -1,4 +1,33 @@
 package com.twu.biblioteca.models;
 
+import java.util.ArrayList;
+
 public class ItemsLibrary {
+
+    public ItemsLibrary(ArrayList<Book> bookList, ArrayList<Movie> movieList) {
+        this.bookList = bookList;
+        this.movieList = movieList;
+    }
+
+    public ItemsLibrary() {
+    }
+
+    private ArrayList<Book> bookList;
+    private ArrayList<Movie> movieList;
+
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public ArrayList<Movie> getMovieList() {
+        return movieList;
+    }
+
+    public void setMovieList(ArrayList<Movie> movieList) {
+        this.movieList = movieList;
+    }
 }
