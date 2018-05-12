b1ea49b200e6d5ef30dda37e3817ada73100760c
NicholasNeto <npnicolls@gmail.com>
2018-05-12 15:47:00 -0300

    add teste para verificar se o usuario esta logado

--- test/com/twu/biblioteca/models/test/UserTest.java
+++ test/com/twu/biblioteca/models/test/UserTest.java
@@ -4,7 +4,10 @@ package com.twu.biblioteca.models.test;
 import com.twu.biblioteca.models.User;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UserTest {
 
@@ -23,5 +26,19 @@ public class UserTest {
         assertEquals("Nicholas@gmail.com", userNicholas.getEmail());
         assertEquals(123456789, userNicholas.getPhone());
         assertEquals("1313", userNicholas.getPassword());
+    }
+
+    @Test
+    public void shouldNotBeLoggedIn(){
+
+        assertFalse(userNicholas.isLogged());
+    }
+
+
+    @Test
+    public void shouldBeLoggedIn() {
+
+        userNicholas.setLogged(true);
+        assertTrue(userNicholas.isLogged());
     }
 }
