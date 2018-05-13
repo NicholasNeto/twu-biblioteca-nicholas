d699d552fb7fb749a7ccfea637399f9866377395
NicholasNeto <npnicolls@gmail.com>
2018-05-13 11:49:20 -0300

    removido function equals de movie

--- src/com/twu/biblioteca/models/Movie.java
+++ src/com/twu/biblioteca/models/Movie.java
@@ -20,19 +20,4 @@ public class Movie extends Item {
         return String.format("%20s %20s %20s %20d %20s", this.getId(), this.getName(), this.getDirector(), this.getYears(), this.getStatusEnum());
     }
 
-    @Override
-    public boolean equals(Object obj) {
-
-        Movie movie = (Movie) obj;
-
-        if (movie.getId().equals(this.getId())
-                && movie.getName().equals(this.getName())
-                && movie.getDirector().equals(this.getDirector())
-                && movie.getYears() == getYears())
-        {
-            return true;
-        }
-            return false;
-
-    }
 }
